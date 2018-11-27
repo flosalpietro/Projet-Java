6cfc8c872730746858e489b2ca239850d6c9c372
He201275 <brahmi-n@hotmail.com>
2018-11-27 22:33:33 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,25 @@
-package MasterM;
+package jeu2;
 
 public class Main {
-	public static void main(String[] args) {
-		Mastermind jeu = new Mastermind();
-		jeu.play();
+public  static  void main(String[] args) {
+		
+		Board b1 = new Board();
+		b1.resultLine.createRandom();
+		b1.propLine.createRandom();
+		
+		for (int i=0;i<Param.NBCASES;i++) {
+			System.out.println(b1.resultLine.getCase(i).getColor());
+		}
+		System.out.println("=====================================");
+		for (int i=0;i<Param.NBCASES;i++) {
+		System.out.println(b1.propLine.getCase(i).getColor());
+		}
+		System.out.println("==================");
+		b1.compare();
+		
+		
+				
 	}
 
 }
+
