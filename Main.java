b724ff1eb8b2f2fb7ef64b85dc3cea7218058d58
BrancartC <HE201330@students.ephec.be>
2018-12-14 15:58:34 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,29 +3,8 @@ package jeu2;
 
 public class Main {
 public  static  void main(String[] args) {
-	boolean win = false;	
-	
-		Board b1 = new Board();
-		b1.resultLine.createRandom();
-		while(!win) {
-		b1.propLine.Prop();
-		
-		
-		
-		for (int i=0;i<Param.NBCASES;i++) {
-			System.out.println(b1.resultLine.getCase(i).getColor());
-		}
-		System.out.println("=====================================");
-		for (int i=0;i<Param.NBCASES;i++) {
-		System.out.println(b1.propLine.getCase(i).getColor());
-		}
-		System.out.println("==================");
-		b1.compare();
-		
-		}
-		
-		
-				
+		Menu menu = new Menu();
+		menu.play();
 	}
 
 }
