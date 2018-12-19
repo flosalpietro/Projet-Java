fa65934626874bfda70205b5c53abc04295d944e
BrancartC <HE201330@students.ephec.be>
2018-12-19 19:54:47 +0100

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,8 +1,7 @@
 
-	
-	package jeu2;
+package jeu2;
 
-	import java.io.BufferedReader;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
