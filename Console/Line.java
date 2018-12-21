ac5c739e656abe7506bb819a2f9f08fab7f01fbc
flosalpietro <florencesalpietro@gmail.com>
2018-12-21 11:14:16 +0100

    organisation

---
+++ Console/Line.java
@@ -0,0 +1,81 @@
+package jeu2;
+
+
+import java.util.Scanner;
+
+public class Line {
+	
+	/**
+	 * Tableau de cases constituant la ligne.
+	 */
+	private Case[] tabCases = new Case[Param.NBCASES];
+	
+	/**
+	 * Constructeur de Line 
+	 */
+	public Line() {
+		for(int i=0;i<Param.NBCASES;i++){
+			tabCases[i] = new Case();
+		}
+		
+		
+		
+	}
+	public void Prop() {
+		System.out.println("Veuillez entrer votre proposition :");
+		Scanner br = new Scanner(System.in); 
+		    String  lines = br.nextLine();    
+		    		
+		    String[] strs = lines.trim().split("(?!^)");
+		if(strs.length!=Param.NBCASES) {
+			System.out.println("veuillez entrer une combinaison à"+Param.NBCASES+" caracteres");
+			Prop();
+		} 
+		
+		try {
+		for(int i=0;i<strs.length;i++){
+			
+			tabCases[i] = new Case(Integer.parseInt(strs[i]));
+			}}
+		catch(NumberFormatException ex){
+				System.out.println("veuillez entrer un chiffre comme caractère");
+			}
+		
+	} 
+	
+
+		/**
+		 * Crée une ligne aléatoire de couleurs.
+		 */
+		public void createRandom(){
+			for(int i=0;i<Param.NBCASES;i++){
+				tabCases[i].randomColor();
+			}
+		}
+		
+		/**
+		 * Renvoie la case souhaitée.
+		 * @param nbCase Le numéro de la case que l'on veut récupérer.
+		 * @return La case demandée en paramètre.
+		 */
+		public Case getCase(int nbCase){
+			return tabCases[nbCase];
+		}
+		
+		/**
+		 * Méthode pour vérifier si la ligne est complète.
+		 * @return true si la ligne est complète, false si il manque une couleur à la ligne.
+		 */
+		public boolean isLineComplete(){
+			int complete=0;
+			for(int i=0;i<Param.NBCASES;i++){
+				if(tabCases[i].getColor()!=0)
+					complete++;
+			}
+			if(complete==Param.NBCASES)return true;
+			return false;
+			
+			}
+			
+		
+		  }
---
+++ Console/Param.java
@@ -0,0 +1,33 @@
+package jeu2;
+
+public class Param {
+	
+	/**
+	 * String des règles du jeu.
+	 */
+	public static String RULES = "<html><p>Le Mastermind est un jeu qui se joue habituellement à deux, le joueur 2 étant l’arbitre du premier.</p><p>Le but du jeu est de deviner une séquence de 4 pions colorés en un minimum d’essai.</p></br><p>Ici c’est légèrement différent, puisque l’ « arbitre » en question, c’est le jeu.</p><p>Dans ce jeu, vous serez capable de jouer à deux, tour à tour.</p><p>Vous serez capable aussi de choisir entre 4 et 6 pions par lignes et 6 à 10 couleurs différentes.</p><p>A chaque tour, Une fois les pions placés, le jeu indique :</p><ul><li>Le nombre de pions de la bonne couleur bien placés en utilisant le même nombre de pions rouges.</li><li>Le nombre de pions de la bonne couleur, mais mal placés, avec les pions blancs.</li></ul></br><p>Le premier à trouver la séquence de pions, sera le gagnant de la partie.</p></html>";
+	
+	
+	public static String RULES_CONS = "<html><p>Le Mastermind est un jeu qui se joue habituellement à deux, mais ici l'on jouera contre le pc.Le but du jeu est de deviner une séquence de 4 pions colorés en moins de 10 essaisIci l’ « arbitre » en question, c’est le jeu.A chaque tour, Une fois les pions placés, le jeu indique:Le nombre de pions de la bonne couleur bien placés en utilisant '+' etle nombre de pions mal placés et/ou de mauvaise couleur par '-' .Si tu trouves la combinaisons en moins de 10 essais tu seras le gagnant de la partie.</p></html>";
+	/**
+	 * Tableau contenant les noms des couleurs dans l'ordre.
+	 */
+	public static String[] COLORS = {"Vide", "Rouge", "Bleu", "Vert", "Orange", "Jaune", "Blanc", "Rose", "Mauve" };
+
+	/**
+	 * Nombre de lignes d'essai pour trouver la réponse.
+	 */
+	public static int NBLINES = 10;
+	
+	/**
+	 * Nombre de couleurs .
+	 */
+	public static int NBCOLORS = 6;
+	
+	/**
+	 * Longueur du code.
+	 */
+	public static int NBCASES = 4;
+	
+	
+}
